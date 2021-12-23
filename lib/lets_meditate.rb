0835d70692c4b8a5a56a17eb3f11058d6aa80652
Saalik Lokhandwala <saalikl111@gmail.com>
2021-12-23 11:41:06 -0600

    add color to text, cleanup

--- lib/lets_meditate.rb
+++ lib/lets_meditate.rb
@@ -1,3 +1,4 @@
+require "colorize"
 require "lets_meditate/timer"
 
 class LetsMeditate
@@ -14,24 +15,23 @@ class LetsMeditate
   private
 
   def self.welcome_message
+    puts `clear`
     puts "Welcome! It's time to meditate."
     puts
-    puts "How many minutes would you like to sit in silence?"
+    puts "How many minutes would you like to sit in silence?".colorize(:light_cyan)
     @minutes = gets.chomp.to_f
   end
   
   def self.trigger_timer
     puts
-    puts "Take a deep breath, inhale and exhale."
-    puts "Press enter when you are ready to begin."
-    puts
+    puts "Press enter when you are ready to begin your meditation.".colorize(:light_cyan)
     gets.chomp
     timer = Timer.new(@minutes)
     timer.countdown
   end
   
   def self.exit_session
-    puts "Press any key to end your session."
+    puts "Press any key to end your session.".colorize(:light_cyan)
     gets.chomp
     exit(true)
   end
--- spec/lets_meditate_spec.rb
+++
@@ -1,5 +0,0 @@
-require "rspec"
-require "lets_meditate"
-
-describe LetsMeditate do
-end
